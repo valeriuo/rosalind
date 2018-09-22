221c18f56d37c79c40f3219f674c581c47716146
Valeriu Ohan <valeriu.ohan@gmail.com>
2018-09-22 12:23:04 +0100

    Complete solution to problem Enumerating Gene Orders (generating permutations using Heap's algorithm)

--- src/_21_PERM.py
+++ src/_21_PERM.py
@@ -1,26 +1,29 @@
-#Enumerating Gene Orders
+#Enumerating Gene Orders (permutations - Heap's algorithm)
 import sys
 
 def heap(n, A):
-    if n == 1: yield A
+    if n == 1: 
+        yield A
     else:
         for i in range(n-1):
-            for hp in heap(n-1, A): yield hp
+            for hp in heap(n-1, A):
+                yield hp
             j = 0 if (n % 2) == 1 else i
             A[j],A[n-1] = A[n-1],A[j]
-        for hp in heap(n-1, A): yield hp
+        for hp in heap(n-1, A):
+            yield hp
 
     
 def main():
-    n = 3
-
-    perms = []
-    for p in heap(n, list(range(n+1))[1:]):
-        print(*p, sep=' ')
-    # print(perms)
-    # print(len(perms))
-    # for p in perms:
-        # print(*p, sep=' ')
+    n = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+    
+    if n > 0:
+        perms = []
+        for p in heap(n, list(range(n+1))[1:]):
+            perms.append(list(p)) 
+        print(len(perms))
+        for p in perms:
+            print(*p, sep=' ')
     
 if __name__ == '__main__':
     main()
