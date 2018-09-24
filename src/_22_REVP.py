265db3493038f7f350d2ef03d5a3411fadce29aa
Valeriu Ohan <valeriu.ohan@gmail.com>
2018-09-24 10:17:08 +0100

    Use a generator for reverse palindromes

--- src/_22_REVP.py
+++ src/_22_REVP.py
@@ -4,10 +4,16 @@ from _3_REVC import revc
 
 def is_revp(input):
     l = len(input)
+
+    return input[:l//2] == revc(input[(l+1)//2:])
+
     
-    if input[:l//2] == revc(input[(l+1)//2:]):
-        return True
-    return False
+def gen_revp(input):
+    l = len(input)
+    for i in range(l-3):
+        for j in range(4, min(12, l-i) + 1, 2):
+            if is_revp(input[i:i+j]):
+                yield (i+1, j)
 
 def main():
     with open('../data/rosalind_revp.txt', 'r') as fp:
@@ -18,20 +24,14 @@ def main():
             else:
                 if len(seq) > 0:
                     # process sequence
-                    l = len(seq)
-                    for i in range(l-3):
-                        for j in range(4, min(12, l-i) + 1, 2):
-                            if is_revp(seq[i:i+j]):
-                                print(i+1, j)
+                    for r in gen_revp(seq):
+                        print(*r)
                     # clear sequence
                     seq = ''
 
         # process last sequence
-        l = len(seq)
-        for i in range(l-3):
-            for j in range(4, min(12, l-i) + 1, 2):
-                if is_revp(seq[i:i+j]):
-                    print(i+1, j)
+        for r in gen_revp(seq):
+            print(*r)
 
 if __name__ == '__main__':
     main()
